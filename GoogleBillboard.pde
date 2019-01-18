0410be80190ed2badf97b300c7c323640f075585
FRESSIA FINN <FRFINN@sfusd.edu>
2019-01-18 07:58:17 -0800

    got rid of extra return statement and bracket

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,3 +1,4 @@
+
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {    
@@ -23,5 +24,3 @@ public boolean isPrime(double dNum)
    return true;  
 } 
 
-   return true;  
-} 
---
+++ build-tmp/source/GoogleBillboard.java
@@ -0,0 +1,51 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
+
+public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public void setup()  
+{    
+	for (int x=2; x< e.length()-10; x++)
+	{	
+		String digits = e.substring(x,x+10);
+    	double dNum = Double.parseDouble(digits);
+    	if(isPrime(dNum) == true)
+    		System.out.println(dNum);
+	}       
+}  
+public void draw()  
+{   
+	//not needed for this assignment
+}  
+public boolean isPrime(double dNum)  
+{   
+   for (double x = 2; x <= Math.sqrt(dNum); x++)
+   {
+   		if (dNum%x == 0)
+   			return false;	
+   } 
+   return true;  
+} 
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
